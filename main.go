13e3427efb1df71a99d90d7e8eb3532c01b8d71b
Wang Yi <1721436982@qq.com>
2023-04-27 11:49:05 +0800

    feat: get addr config

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/AlecAivazis/survey/v2"
+	"gopkg.in/gomail.v2"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"os"
@@ -13,7 +14,7 @@ import (
 
 type Addr struct {
 	Host string `yaml:"host"`
-	Port int64  `yaml:"port"`
+	Port int    `yaml:"port"`
 }
 
 type TplConfig struct {
@@ -26,11 +27,6 @@ type Config struct {
 	Email    string `yaml:"email"`
 	Password string `yaml:"password"`
 }
-
-var (
-	host = "smtp.qq.com"
-	port = 25
-)
 
 func questionsConfig() (*Config, error) {
 	var config Config
@@ -58,6 +54,25 @@ func questionsConfig() (*Config, error) {
 	return &config, nil
 }
 
+func questionTpl(options []TplConfig) *TplConfig {
+	var names []string
+	for _, o := range options {
+		names = append(names, o.Name)
+	}
+	var name string
+	question := &survey.Select{
+		Message: "请选择你要发送的模版",
+		Options: names,
+	}
+	_ = survey.AskOne(question, &name)
+	for _, o := range options {
+		if name == o.Name {
+			return &o
+		}
+	}
+	return nil
+}
+
 func setEmailConfig(c *Config) {
 	out, _ := yaml.Marshal(c)
 	wd := getDw()
@@ -79,17 +94,51 @@ func getEmailConfig() (*Config, error) {
 	return &config, err
 }
 
-func getTplOptions() []string {
-	dw := getDw()
-	dir := filepath.Join(dw, "tpl", "*")
-	fmt.Printf("dir: %s\n", dir)
-	files, _ := filepath.Glob(dir)
-	return files
+func getAddrConfig() *Addr {
+	wd := getDw()
+	var config Addr
+	content, err := os.ReadFile(fmt.Sprintf("%s/addr.yaml", wd))
+	if err != nil {
+		panic("获取Addr失败")
+	}
+	err = yaml.Unmarshal(content, &config)
+	if err != nil {
+		panic("解析Addr失败")
+	}
+	return &config
 }
 
-func getTpl(filename string) string {
-	wd := getDw()
-	content, _ := os.ReadFile(fmt.Sprintf("%s/tpl/%s", wd, filename))
+// 只获取.html文件
+func filterHtmlFile(files []string) (filterFiles []string) {
+	for _, file := range files {
+		if strings.Contains(file, ".html") {
+			filterFiles = append(filterFiles, file)
+		}
+	}
+	return filterFiles
+}
+
+func getTplOptions() []TplConfig {
+	dw := getDw()
+	dir := filepath.Join(dw, "tpl", "*")
+	files, _ := filepath.Glob(dir)
+	files = filterHtmlFile(files)
+	var options []TplConfig
+	for _, file := range files {
+		arr := strings.Split(file, "/")
+		filename := arr[len(arr)-1]
+		name := strings.Split(filename, ".")[0]
+		options = append(options, TplConfig{
+			Path:     file,
+			Filename: filename,
+			Name:     name,
+		})
+	}
+	return options
+}
+
+func getTpl(path string) string {
+	content, _ := os.ReadFile(path)
 	return string(content)
 }
 
@@ -112,20 +161,9 @@ func isRun() bool {
 	// 获取调用者的文件路径和行号
 	_, callerFile, _, _ := runtime.Caller(0)
 
-	// 获取可执行文件的路径
-	execFile, err := os.Executable()
-	if err != nil {
-		panic(err)
-	}
-
 	// 比较调用者文件和可执行文件的路径
 	if filepath.Base(callerFile) == "main.go" {
-		fmt.Println("程序是通过 go run 命令启动的")
 		return true
-	} else if filepath.Base(callerFile) == filepath.Base(execFile) {
-		fmt.Println("程序是通过编译后的可执行文件启动的")
-	} else {
-		fmt.Println("无法确定程序是如何启动的")
 	}
 	return false
 }
@@ -137,19 +175,20 @@ func main() {
 		setEmailConfig(c)
 	}
 	tplOptions := getTplOptions()
-	fmt.Printf("tpls: %s\n", tplOptions)
-	//emails := getEmails()
-	//for _, to := range emails {
-	//	m := gomail.NewMessage()
-	//	m.SetHeader("From", c.Email)
-	//	m.SetHeader("To", to)
-	//	m.SetHeader("Subject", "Subject")
-	//	body := getTpl("welcome.html")
-	//	m.SetBody("text/html", body)
-	//	d := gomail.NewDialer("smtp.qq.com", 25, c.Email, c.Password)
-	//
-	//	if err := d.DialAndSend(m); err != nil {
-	//		fmt.Printf("%s 邮件发送失败\n", to)
-	//	}
-	//}
+	tpl := questionTpl(tplOptions)
+	emails := getEmails()
+	addr := getAddrConfig()
+	for _, to := range emails {
+		m := gomail.NewMessage()
+		m.SetHeader("From", c.Email)
+		m.SetHeader("To", to)
+		m.SetHeader("Subject", tpl.Name)
+		body := getTpl(tpl.Path)
+		m.SetBody("text/html", body)
+		d := gomail.NewDialer(addr.Host, addr.Port, c.Email, c.Password)
+
+		if err := d.DialAndSend(m); err != nil {
+			fmt.Printf("%s 邮件发送失败\n", to)
+		}
+	}
 }
